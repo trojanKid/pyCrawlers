eee129aecbe9f321c660b84cbacd6ec3f4e90c05
Shiji Zhou <shijizho@usc.edu>
2018-02-10 13:59:34 -0800

    init files

---
+++ myFirstScrapy/myFirstScrapy/spiders/mySpider.py
@@ -0,0 +1,29 @@
+import scrapy
+import re
+from bs4 import BeautifulSoup
+from scrapy.http import Request
+from myFirstScrapy.items import MyfirstscrapyItem
+
+
+class mySpider(scrapy.Spider):
+    name = 'myspider'
+
+    def start_requests(self):
+        urls = [
+            'http://quotes.toscrape.com/page/1/',
+            'http://quotes.toscrape.com/page/2/',
+        ]
+        for url in urls:
+            yield Request(url=url, callback=self.parse)
+
+    def parse(self, response):
+        item = MyfirstscrapyItem()
+        authors = BeautifulSoup(response.text, 'lxml').find_all('small', itemprop='author')
+        quotes = BeautifulSoup(response.text, 'lxml').find_all('span', itemprop="text")
+        nums = len(authors)
+        for i in range(0, nums-1):
+            item['author'] = authors[i].get_text()
+            print(authors[i].get_text())
+            item['quote'] = quotes[i].get_text()
+            print(quotes[i].get_text())
+            yield item
